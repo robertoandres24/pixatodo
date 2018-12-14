02a614c4b32551ca985904b0878763958fe7e20d
roberto obando <robertoandres24@gmail.com>
2018-12-14 20:20:27 -0300

    initialize template vuejs chrome

---
+++ src/popup/router/pages/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>Hello beautiful world!</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+p {
+  font-size: 20px;
+}
+</style>

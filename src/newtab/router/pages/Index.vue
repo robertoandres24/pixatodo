4531fdde54c7344c5a377cdb2913ed4cac424651
roberto obando <robertoandres24@gmail.com>
2018-12-14 23:31:16 -0300

    get new image when reloading

--- src/newtab/router/pages/Index.vue
+++ src/newtab/router/pages/Index.vue
@@ -1,14 +1,51 @@
 <template>
   <div class="main-screen">
-    <div class="hero-bg"></div>
-    <div class="change-bg-icon"></div>
+    <div class="hero-bg" :style="{ background: `radial-gradient(transparent, black), center / cover no-repeat url('${image}')` }"></div>
+    <div @click="getRandomBg()" class="change-bg-icon"></div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 export default {
   data() {
-    return {};
+    return {
+      image: '',
+    };
+  },
+  watch: {
+    // whenever question changes, this function will run
+    image: function(newImage, oldImage) {
+      console.log(newImage, oldImage);
+    },
+  },
+  computed: {
+    randomNumber() {
+      return Math.floor(Math.random() * 200) + 1;
+    },
+  },
+  mounted() {
+    this.getRandomBg();
+  },
+  methods: {
+    getRandomBg() {
+      console.log('clicked');
+      // Optionally the request above could also be done as
+      axios
+        .get('https://pixabay.com/api/', {
+          params: {
+            key: '11002686-685122bed59c07caf3ac56d3f',
+            per_page: 200,
+          },
+        })
+        .then(response => {
+          console.log(response.data.hits);
+          this.image = response.data.hits[this.randomNumber].largeImageURL;
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    },
   },
 };
 </script>
@@ -20,9 +57,6 @@ export default {
   bottom: 0;
   left: 0;
   right: 0;
-  background: radial-gradient(transparent, black), url('/static/images/hero-bg.jpg');
-  background-size: cover;
-  background-position: center;
 }
 .change-bg-icon {
   background: url('/static/images/change-bg-icon.svg');
